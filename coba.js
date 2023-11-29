9c09db575d395889e7fb3be97ea4eb62996393f6
mustikap08 <mustikaputriw08@gmail.com>
2023-11-29 13:03:11 +0700

    first commit

---
+++ coba.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 1,
+  duration: "30s",
+  thresholds: {
+    http_req_duration: ["p(99)<1500"],
+  },
+};
+
+export default function () {
+  http.get('https://jti.polinema.ac.id/');
+  sleep(1);
+}
