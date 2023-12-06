53a6ed69e02e6b058d52bd21ac8910a642038071
mustikap08 <mustikaputriw08@gmail.com>
2023-12-06 09:38:12 +0700

    ub

---
+++ ub.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 1,
+  duration: "30s",
+  thresholds: {
+    http_req_duration: ["p(99)<1500"],
+  },
+};
+
+export default function () {
+  http.get('https://ub.ac.id/id/');
+  sleep(1);
+}
